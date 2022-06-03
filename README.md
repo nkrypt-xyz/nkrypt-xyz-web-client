997d3f762c800476bd0c3ce90415a1ca63129460
Sayem Shafayet <ishafayet@gmail.com>
2022-06-04 00:15:34 +0600

    Bootstrap svelte app with temporary logo

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # nkrypt-xyz-web-client
+
 User deployable browser client for nkrypt.xyz community edition.
+
+## To run
+
+```
+npm i
+npm run dev
+```
---
+++ src/lib/Example.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import Button, { Label, Icon } from "@smui/button";
+  import { CommonConstant } from "../constant/common-constants.js";
+
+  const clicked = () => {
+    console.log("Hi");
+  };
+</script>
+
+<div>App version: {CommonConstant.APP_VERSION}</div>
+
+<Button on:click={clicked}>
+  <Icon class="material-icons">favorite</Icon>
+  <Label>Example Button</Label>
+</Button>
+
+<style>
+</style>
