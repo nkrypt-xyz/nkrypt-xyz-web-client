b05d420e8f9b381eac328a4b5057718ecff53b47
Sayem Shafayet <ishafayet@gmail.com>
2022-06-04 00:15:34 +0600

    Bootstrap svelte app with temporary logo

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import logo from "./asset/logo-512-sqr.png";
+  import Example from "./lib/Example.svelte";
+</script>
+
+<main>
+  <img src={logo} alt="logo-512-sqr" />
+  <h1>Test</h1>
+  <Example />
+</main>
+
+<style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+      Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  }
+</style>
