d8817bc0b87eede87dfba3fe2b37370f569c8c0d
Sayem Shafayet <ishafayet@gmail.com>
2022-06-15 21:55:09 +0600

    Add minimal routing and theming

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,62 @@
 <script lang="ts">
   import logo from "./asset/logo-512-sqr.png";
   import Example from "./lib/Example.svelte";
+
+  import Router, { replace } from "svelte-spa-router";
+  import { wrap } from "svelte-spa-router/wrap";
+
+  import LoginPage from "./page/LoginPage.svelte";
+  import DashboardPage from "./page/DashboardPage.svelte";
+
+  import { currentUser } from "./store/user.js";
+
+  const conditionRequiresAuthentication = async (detail) => {
+    if ((detail.userData as any).requiresAuthentication) {
+      console.log($currentUser);
+      if (!$currentUser) {
+        detail.userData.isUserLoggedIn = false;
+        return false;
+      }
+    }
+    return true;
+  };
+
+  const routes = {
+    "/dashboard": wrap({
+      component: DashboardPage,
+      userData: {
+        requiresAuthentication: true,
+      },
+      conditions: [conditionRequiresAuthentication],
+    }),
+    "/login": wrap({
+      component: LoginPage,
+    }),
+  };
+
+  // Handles the "conditionsFailed" event dispatched by the router when a component can't be loaded because one of its pre-condition failed
+  function conditionsFailed(event) {
+    console.error("conditionsFailed event", event.detail);
+    let data = event.detail.userData as any;
+
+    if (data.requiresAuthentication && !data.isUserLoggedIn) {
+      delete data.isUserLoggedIn;
+      replace("/login");
+    }
+  }
+
+  // Handles the "routeLoaded" event dispatched by the router when a component was loaded
+  function routeLoaded(event) {
+    console.log("routeLoaded event", event.detail);
+  }
 </script>
 
 <main>
-  <img src={logo} alt="logo-512-sqr" />
-  <h1>Test</h1>
-  <Example />
+  <Router
+    {routes}
+    on:conditionsFailed={conditionsFailed}
+    on:routeLoaded={routeLoaded}
+  />
 </main>
 
 <style>
