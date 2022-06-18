c2eb68a6c5f77a7b04d1e81e4ede67bcd088a1b7
Sayem Shafayet <ishafayet@gmail.com>
2022-06-18 21:43:46 +0600

    Add change observer

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { logRelevantFileChanges } from "./devops/vite/change-observer.js";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [logRelevantFileChanges(), svelte()],
   publicDir: "public",
+  logLevel: "error",
 });
