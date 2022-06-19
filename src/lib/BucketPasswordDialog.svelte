1b2fb9313b3af0c75b9256719879ce9ea916e867
Sayem Shafayet <ishafayet@gmail.com>
2022-06-20 00:44:19 +0600

    Collect password from user

---
+++ src/lib/BucketPasswordDialog.svelte
@@ -0,0 +1,93 @@
+<script lang="ts">
+  // UI
+  import Footer from "../lib/Footer.svelte";
+  import Textfield from "@smui/textfield";
+  import Icon from "@smui/textfield/icon";
+  import HelperText from "@smui/textfield/helper-text";
+  // Extern
+  import { form, field } from "svelte-forms";
+  import { required, min } from "svelte-forms/validators";
+  import { replace } from "svelte-spa-router";
+  // Intern
+  import { standardField } from "../lib/validations.js";
+  import { CommonConstant } from "../constant/common-constants.js";
+  import { defaultServerToShow } from "../store/cache.js";
+  import { callUserLoginApi } from "../integration/user-apis.js";
+  import { minlength } from "../lib/validators.js";
+  import { extract } from "../utility/misc-utils.js";
+  import { currentUser } from "../store/user.js";
+  import { currentSession } from "../store/session.js";
+  import {
+    decrementActiveGlobalObtrusiveTaskCount,
+    incrementActiveGlobalObtrusiveTaskCount,
+  } from "../store/ui.js";
+  import {
+    callBucketCreateApi,
+    callBucketListApi,
+  } from "../integration/content-apis.js";
+  import { encryptText } from "../utility/crypto-utils.js";
+  import { BUCKET_CRYPTO_SPEC } from "../lib/crypto.js";
+  import { bucketList } from "../store/content.js";
+
+  import Dialog, { Title, Content, Actions } from "@smui/dialog";
+  import Button, { Label } from "@smui/button";
+  import CircularProgress from "@smui/circular-progress";
+
+  import {
+    bucketPasswordDialog,
+    bucketPasswordDialogResponse,
+  } from "../store/ui.js";
+
+  const encryptionPassword = standardField(
+    "encryptionPassword",
+    "PleaseChangeMe@YourEarliest2Day",
+    [minlength(8)]
+  );
+  const finalForm = form(encryptionPassword);
+
+  let _bucketPasswordDialog = null;
+  bucketPasswordDialog.subscribe((value) => {
+    _bucketPasswordDialog = value;
+  });
+
+  const setAnswer = (answer) => {
+    bucketPasswordDialog.set(null);
+    bucketPasswordDialogResponse.set(answer);
+  };
+</script>
+
+{#if _bucketPasswordDialog}
+  <Dialog
+    bind:open={_bucketPasswordDialog}
+    scrimClickAction=""
+    escapeKeyAction=""
+    aria-labelledby="mandatory-title"
+    aria-describedby="mandatory-content"
+  >
+    <Title id="mandatory-title">
+      Password for bucket: {_bucketPasswordDialog.bucketName}
+    </Title>
+    <Content id="mandatory-content">
+      <Textfield
+        bind:value={$encryptionPassword.value}
+        label="Encryption password"
+        type="password"
+        required
+      >
+        <Icon class="material-icons" slot="leadingIcon">key</Icon>
+        <HelperText slot="helper"
+          >This is separate from your login password and is specific to this
+          bucket
+        </HelperText>
+      </Textfield>
+    </Content>
+    <Actions>
+      <Button on:click={() => setAnswer(false)}>
+        <Label>Cancel</Label>
+      </Button>
+      <Button on:click={() => setAnswer($encryptionPassword.value)}>
+        <Label>Okay</Label>
+      </Button>
+    </Actions>
+  </Dialog>
+{/if}
---
+++ src/store/password.ts
@@ -0,0 +1,34 @@
+import {
+  getLocalStorageItem,
+  setLocalStorageItem,
+} from "../utility/store-utils.js";
+import { Writable, writable } from "svelte/store";
+import type { Session } from "../model/common.js";
+
+const BUCKET_PREFIX = "bucket_";
+const SAVED_PASSWORDS_LOCALSTORAGE_KEY = "--saved-passwords";
+let _storedPassword = {};
+
+const storedPassword: Writable<Record<string, any>> = writable(
+  getLocalStorageItem(SAVED_PASSWORDS_LOCALSTORAGE_KEY) || {}
+);
+
+storedPassword.subscribe((value) => {
+  _storedPassword = value;
+  setLocalStorageItem(SAVED_PASSWORDS_LOCALSTORAGE_KEY, value);
+});
+
+export const getPasswordForBucket = (bucketId) => {
+  let key = BUCKET_PREFIX + bucketId;
+  if (key in _storedPassword) {
+    return _storedPassword[key];
+  }
+};
+
+export const setPasswordForBucket = (bucketId, encryptionPassword) => {
+  let key = BUCKET_PREFIX + bucketId;
+  storedPassword.update((map) => {
+    map[key] = encryptionPassword;
+    return map;
+  });
+};
