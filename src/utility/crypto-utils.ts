3d67283e0e25d2bba682614c0a21ba35261dc4cd
Sayem Shafayet <ishafayet@gmail.com>
2022-06-20 00:43:01 +0600

    Add decryptText

--- src/utility/crypto-utils.ts
+++ src/utility/crypto-utils.ts
@@ -18,8 +18,13 @@ const convertSmallStringToBuffer = (packed: string) => {
   return buffer;
 };
 
-const createEncryptionKeyFromPassword = async (encryptionPassword: string) => {
-  const salt = window.crypto.getRandomValues(new Uint8Array(16));
+const createEncryptionKeyFromPassword = async (
+  encryptionPassword: string,
+  salt: Uint8Array
+) => {
+  if (!salt) {
+    salt = window.crypto.getRandomValues(new Uint8Array(16));
+  }
 
   let encodedPassphrase = new TextEncoder().encode(encryptionPassword);
 
@@ -51,10 +56,13 @@ const createEncryptionKeyFromPassword = async (encryptionPassword: string) => {
 export const encryptText = async (text: string, encryptionPassword: string) => {
   // encode
   const encoder = new TextEncoder();
-  let encodedData = encoder.encode(encryptionPassword);
+  let encodedData = encoder.encode(text);
 
   // get salt and key
-  let { salt, key } = await createEncryptionKeyFromPassword(encryptionPassword);
+  let { salt, key } = await createEncryptionKeyFromPassword(
+    encryptionPassword,
+    null
+  );
 
   // generate random iv
   let iv = window.crypto.getRandomValues(new Uint8Array(12));
@@ -68,9 +76,38 @@ export const encryptText = async (text: string, encryptionPassword: string) => {
     encodedData
   );
 
+  // pack
   return {
     cipher: convertSmallBufferToString(cipher),
     iv: convertSmallUint8ArrayToString(iv),
     salt: convertSmallUint8ArrayToString(salt),
   };
+};
+
+// For decrypting small (<10mb) amount of data
+export const decryptText = async (
+  { cipher, iv, salt },
+  encryptionPassword: string
+) => {
+  // unpack
+  cipher = convertSmallStringToBuffer(cipher);
+  iv = convertSmallStringToBuffer(iv);
+  salt = convertSmallStringToBuffer(salt);
+
+  // get  key
+  let { key } = await createEncryptionKeyFromPassword(encryptionPassword, salt);
+
+  const encodedData = await window.crypto.subtle.decrypt(
+    {
+      name: "AES-GCM",
+      iv: iv,
+    },
+    key,
+    cipher
+  );
+
+  // decode
+  let data = new TextDecoder().decode(encodedData);
+
+  return data;
 };
