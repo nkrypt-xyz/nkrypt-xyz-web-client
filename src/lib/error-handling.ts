c6e135d32d89a2612615eecebe426df9f227bc3c
Sayem Shafayet <ishafayet@gmail.com>
2022-06-20 02:28:18 +0600

    Logout user on apiKey related error

--- src/lib/error-handling.ts
+++ src/lib/error-handling.ts
@@ -2,16 +2,24 @@ import {
   decrementActiveGlobalObtrusiveTaskCount,
   showAlert,
 } from "../store/ui.js";
+import { performUserLogout } from "./session.js";
 
 export const handleErrorIfAny = async (
   response,
   reduceObtrusiveLoader: boolean = true
 ): Promise<boolean> => {
   if (!response.hasError) return false;
-  let { message } = response.error;
+  let { message, code } = response.error;
   await showAlert("Error occurred", message);
+
   if (reduceObtrusiveLoader) {
     decrementActiveGlobalObtrusiveTaskCount();
   }
+
+  const apiKeyErrorCodeList = ["API_KEY_NOT_FOUND", "API_KEY_EXPIRED"];
+  if (apiKeyErrorCodeList.indexOf(code) > -1) {
+    await performUserLogout();
+  }
+
   return true;
 };
