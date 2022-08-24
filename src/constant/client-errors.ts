5b14ee12df80a29bcc8179eaaa6634db04dfe2d1
Sayem Shafayet <ishafayet@gmail.com>
2022-08-24 22:47:34 +0600

    Add basic error handling during file download

---
+++ src/constant/client-errors.ts
@@ -0,0 +1,7 @@
+export const clientError = {
+  DECRYPTION_FAILED: {
+    code: "DECRYPTION_FAILED",
+    message: "Decryption failed",
+  },
+  DOWNLOAD_FAILED: { code: "DOWNLOAD_FAILED", message: "Downlaod failed" },
+};
