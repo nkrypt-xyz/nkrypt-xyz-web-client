6f443bbb920a6661ef0dde3d198edcc724f18d22
Sayem Shafayet <ishafayet@gmail.com>
2022-08-27 05:01:43 +0600

    Implement working stream based downloads with metered chunks #6

--- src/lib/crypto-transit-stream.ts
+++ src/lib/crypto-transit-stream.ts
@@ -1,5 +1,8 @@
 import { convertSmallUint8ArrayToString } from "../utility/buffer-utils.js";
-import { buildCryptoHeader } from "../utility/crypto-api-utils.js";
+import {
+  buildCryptoHeader,
+  unbuildCryptoHeader,
+} from "../utility/crypto-api-utils.js";
 import {
   callBlobReadStreamApi,
   callBlobWriteStreamApi,
@@ -18,9 +21,11 @@ import {
 } from "../utility/stream-and-buffer-utils.js";
 import {
   BLOB_CHUNK_SIZE_BYTES,
+  ENCRYPTION_TAGLENGTH_IN_BITS,
   IV_LENGTH,
   SALT_LENGTH,
 } from "./crypto-specs.js";
+import { handleErrorIfAny } from "./error-handling.js";
 
 const createCipherProperties = async (bucketPassword: string) => {
   let { iv } = await makeRandomIv();
@@ -101,87 +106,37 @@ export const encryptAndUploadFile = async (
   return response;
 };
 
-// First IV_LENGTH bytes are IV and next SALT_LENGTH bytes are salt
 const createDeryptedPseudoTransformStream = async (
   inputStream: ReadableStream,
-  bucketPassword: string,
+  { iv, key },
+  contentLengthOnServer,
   progressNotifierFn: Function
 ): Promise<ReadableStream<any>> => {
-  let salt: Uint8Array = null;
-  let iv: Uint8Array = null;
-  let key: CryptoKey = null;
+  const totalBytes = contentLengthOnServer;
+  let bytesRead = 0;
+  progressNotifierFn(totalBytes, 0, 0);
 
-  // const totalBytes = file.size;
-  // let bytesRead = 0;
-  // progressNotifierFn(totalBytes, bytesRead, 0);
-
-  let inputStreamReader = inputStream.getReader();
+  let meteredBytedReader = new MeteredByteStreamReader(inputStream);
 
   // Note: We are not using transform streams due to a lack of browser support.
   return new ReadableStream({
     async pull(controller) {
-      let {
-        value: encryptedChunk,
-        done,
-      }: { value?: Uint8Array | ArrayBuffer; done: boolean } =
-        await inputStreamReader.read();
+      const { value: chunk, done } = await meteredBytedReader.readBytes(
+        BLOB_CHUNK_SIZE_BYTES + ENCRYPTION_TAGLENGTH_IN_BITS / 8
+      );
 
       if (done) {
         controller.close();
         return;
       }
 
-      if (!encryptedChunk) {
-        throw new Error("Expected encryptedChunk");
-      }
+      let chunkBuffer: ArrayBuffer = chunk.buffer;
+      let decryptedChunkBuffer = await decryptBuffer({ iv, key }, chunkBuffer);
 
-      if (encryptedChunk instanceof ArrayBuffer) {
-        console.log(
-          "Surprisingly got ArrayBuffer where Uint8Array was expected. Moving on."
-        );
-      } else if (encryptedChunk instanceof Uint8Array) {
-        // convert to ArrayBuffer
-        encryptedChunk = encryptedChunk.buffer;
-      } else {
-        console.log("Invalid type", encryptedChunk);
-        throw new Error("Expected encryptedChunk to be an Uint8Array.");
-      }
+      bytesRead += chunkBuffer.byteLength;
+      progressNotifierFn(totalBytes, bytesRead, bytesRead);
 
-      if (!salt && !iv) {
-        // This is the first ever transmission
-        console.log("FIRST DOWN COMBINED encryptedChunk", encryptedChunk);
-
-        let encryptedChunkView = new Uint8Array(encryptedChunk);
-
-        if (encryptedChunkView.length < IV_LENGTH + SALT_LENGTH) {
-          throw new Error(
-            "Unexpected edge case. Did not expect chunk to be so small"
-          );
-        }
-
-        iv = encryptedChunkView.slice(0, IV_LENGTH);
-        salt = encryptedChunkView.slice(IV_LENGTH, IV_LENGTH + SALT_LENGTH);
-        ({ key } = await createEncryptionKeyFromPassword(bucketPassword, salt));
-
-        let newEncryptedChunkView = encryptedChunkView.slice(
-          IV_LENGTH + SALT_LENGTH,
-          encryptedChunkView.length
-        );
-        let newEncryptedChunk = newEncryptedChunkView.buffer;
-
-        console.log("FIRST DOWN iv", iv);
-        console.log("FIRST DOWN salt", salt);
-        console.log("FIRST DOWN REDUCED encryptedChunk", newEncryptedChunk);
-
-        let decryptedChunk = await decryptBuffer(
-          { iv, key },
-          newEncryptedChunk
-        );
-        controller.enqueue(decryptedChunk);
-      } else {
-        let decryptedChunk = await decryptBuffer({ iv, key }, encryptedChunk);
-        controller.enqueue(decryptedChunk);
-      }
+      controller.enqueue(decryptedChunkBuffer);
     },
   });
 };
@@ -212,6 +167,12 @@ export const downloadAndDecryptFile = async (
   progressNotifierFn: Function
 ) => {
   let response = await callBlobReadStreamApi(bucketId, fileId);
+  if (await handleErrorIfAny(response)) return null;
+
+  let { readableStream, cryptoHeaderContent, contentLengthOnServer } = response;
+
+  let [iv, salt] = unbuildCryptoHeader(cryptoHeaderContent);
+  let { key } = await createEncryptionKeyFromPassword(bucketPassword, salt);
 
   if (response.hasError) {
     return response;
@@ -219,7 +180,8 @@ export const downloadAndDecryptFile = async (
 
   let decryptedReadableStream = await createDeryptedPseudoTransformStream(
     response.readableStream,
-    bucketPassword,
+    { iv, key },
+    contentLengthOnServer,
     progressNotifierFn
   );
 
