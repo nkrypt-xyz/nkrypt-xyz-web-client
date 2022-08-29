f070604b263532526118f9c00fb1c01ec359b952
Sayem Shafayet <ishafayet@gmail.com>
2022-08-30 01:51:34 +0600

    Implement quantized stream upload #6

--- src/constant/common-constants.ts
+++ src/constant/common-constants.ts
@@ -1,6 +1,9 @@
+import { BLOB_CHUNK_SIZE_INCLUDING_TAG_BYTES } from "./crypto-specs.js";
+
 export const CommonConstant = {
   APP_VERSION: "0.0.1",
   COPYRIGHT: "Sayem Shafayet",
   COPYRIGHT_HREF: "https://ishafayet.me",
   DEFAULT_SERVER_URL: "http://localhost:9041",
+  PACKET_SIZE_FOR_QUANTIZED_STREAMS: 100 * BLOB_CHUNK_SIZE_INCLUDING_TAG_BYTES,
 };
