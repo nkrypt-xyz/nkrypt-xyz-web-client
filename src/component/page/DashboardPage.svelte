0a5b1ab254bf67a95e6dce3951d1e39308dfd21e
Sayem Shafayet <ishafayet@gmail.com>
2022-11-28 00:39:38 +0600

    Add metrics to dashboard

--- src/component/page/DashboardPage.svelte
+++ src/component/page/DashboardPage.svelte
@@ -1,23 +1,90 @@
 <script lang="ts">
   // UI / Framework
   import Button, { Icon, Label } from "@smui/button";
+  import { storedUser } from "../../store/user.js";
+  import { callMetricsGetSummaryApi } from "../../integration/metrics-apis.js";
+  import { handleAnyError } from "../../lib/error-handling.js";
   // Other imports
-  import { showAlert, showConfirmation } from "../../store/ui.js";
+  import {
+    decrementActiveGlobalObtrusiveTaskCount,
+    incrementActiveGlobalObtrusiveTaskCount,
+    showAlert,
+    showConfirmation,
+  } from "../../store/ui.js";
 
-  const clicked = async () => {
-    let ans = await showConfirmation("Example Title", "Example Text");
-    await showAlert("Another Title", "Another Text");
+  let metrics = null;
+
+  const loadMetrics = async () => {
+    try {
+      incrementActiveGlobalObtrusiveTaskCount();
+      let response = await callMetricsGetSummaryApi({});
+      decrementActiveGlobalObtrusiveTaskCount();
+      metrics = response;
+      return response;
+    } catch (ex) {
+      return await handleAnyError(ex);
+    }
   };
+
+  const formatSize = (bytes) => {
+    let value = Math.round(((bytes || 0) / 1000 / 1000 / 1000) * 100) / 100;
+    return `${value} GB`;
+  };
+
+  loadMetrics();
 </script>
 
-<div class="nk-page">
-  DASHBOARD <br />
+<div class="nk-page dashboard">
+  Welcome, {$storedUser.displayName}
 
-  <Button on:click={clicked}>
-    <Icon class="material-icons">favorite</Icon>
-    <Label>Example Button</Label>
-  </Button>
+  {#if metrics && metrics.disk}
+    <div class="section">
+      <div class="title">Disk Usage</div>
+
+      <div class="item">
+        <div class="label">Used</div>
+        <div class="value">
+          {formatSize(metrics.disk.usedBytes)}
+        </div>
+      </div>
+
+      <div class="item">
+        <div class="label">Total</div>
+        <div class="value">
+          {formatSize(metrics.disk.totalBytes)}
+        </div>
+      </div>
+    </div>
+  {/if}
 </div>
 
 <style>
+  .dashboard {
+    padding: 8px;
+  }
+
+  .section {
+    margin-top: 6px;
+  }
+
+  .section .title {
+    background-color: #2a8dc7;
+    color: white;
+    padding: 8px;
+  }
+
+  .section .item {
+    display: flex;
+    padding: 4px;
+
+    background-color: #bbd6e6;
+  }
+
+  .section .item .label {
+    flex: 1;
+  }
+
+  .section .item .value {
+    flex: 2;
+  }
 </style>
