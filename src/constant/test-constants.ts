fa40b5e8682deb8416d46e4cc38eaa0ff578027b
Sayem Shafayet <ishafayet@gmail.com>
2022-12-19 14:09:12 +0600

    Disable WEAKEN_CRYPTO_FOR_TESTING flag

--- src/constant/test-constants.ts
+++ src/constant/test-constants.ts
@@ -1,5 +1,5 @@
 export const testConstants = {
-  WEAKEN_CRYPTO_FOR_TESTING: true,
+  WEAKEN_CRYPTO_FOR_TESTING: false,
   TEST_IV: new Uint8Array([
     1, 240, 129, 77, 139, 24, 34, 249, 37, 144, 177, 111,
   ]),
