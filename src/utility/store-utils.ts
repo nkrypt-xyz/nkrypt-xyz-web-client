b61acea807ec91987f67c52e58625156935d9716
Sayem Shafayet <ishafayet@gmail.com>
2022-12-26 01:55:37 +0600

    Store back button details on SessionStorage #22

--- src/utility/store-utils.ts
+++ src/utility/store-utils.ts
@@ -10,3 +10,16 @@ export const getLocalStorageItem = (key) => {
   }
   return value;
 };
+
+export const setTabStorageItem = (key, value) => {
+  value = JSON.stringify(value);
+  sessionStorage.setItem(key, value);
+};
+
+export const getTabStorageItem = (key) => {
+  let value = sessionStorage.getItem(key);
+  if (value) {
+    return JSON.parse(value);
+  }
+  return value;
+};
